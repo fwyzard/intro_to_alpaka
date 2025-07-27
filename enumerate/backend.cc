3423ee58e17864817d64bd470789977b2af38a7c
Andrea Bocci <andrea.bocci@cern.ch>
2025-07-27 07:39:41 +0200

    Improve enumarate message

--- enumerate/backend.cc
+++ enumerate/backend.cc
@@ -30,7 +30,13 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
     std::vector<Device> devices = alpaka::getDevs(platform);
 
     std::cout << "Accelerator platform: " << alpaka::core::demangled<Platform> << '\n';
-    std::cout << "Found " << devices.size() << " device(s):\n";
+    if (devices.empty()) {
+      std::cout << "No devices found\n";
+    } else if (devices.size() == 1) {
+      std::cout << "Found 1 device:\n";
+    } else {
+      std::cout << "Found " << devices.size() << " device(s):\n";
+    }
     for (auto const& device : devices) {
       std::cout << "  - " << alpaka::getName(device) << '\n';
       if (verbose) {
